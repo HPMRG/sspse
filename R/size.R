c37846cb345b9144655c87ae87297d4c571f1a94
Mark S. Handcock <handcock@ucla.edu>
2009-02-15 10:45:09 +0000

    Substantial update adding, in particular, "margposteriorsize" to compute the marginal posterior distribution of population size, and "discretemle" to compute the profile likelihood of the population size.
    "margposteriorsize" uses a rejection sampler and appears to be algorithmically and computationally correct. However, the solutions are not credible, so I will need to think about it some more.
    
    "discretemle" maximizes over a (given) set of population sizes using a mixture of optim() to get close in the continuous space of class sizes and then a sampler to search locally for the discrete set of class sizes that is the maximum. This seems to work well.
    
    Both exploit parallalization using PVM/MPI via the "parallel=" argument. This speeds up computation considerable and works on mosix and student.
    
    This commit os for the R side.
    
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/rdsworkinggroup/software/size@101 adfe0e35-f9fe-4921-b9ec-9a706e04b580

--- R/size.R
+++ R/size.R
@@ -1,65 +1,22 @@
-margposteriorsize<-function(s,N=trunc(length(s)*seq(1.1,4,length=10)+1),
-          K=(1:max(s)),
+margposteriorsize<-function(N=trunc(length(s)*seq(1.1,4,length=10)+1),
+          s,
+	  K=max(s),
           prob=rep(1/K,K),
           M=100000,
           parallel=1,seed=NULL, verbose=TRUE, 
           n=tabulate(s,nbin=K),
-          lbound=NULL,
-          nstart=NULL,
           return.all=TRUE){
   if(length(prob)!=K){
-    stop("N needs to be a vector of integers.")
+    stop("The vector of probabilities need to have length K.")
   }
   if(length(N)==0||!is.numeric(N)||trunc(N)!=N){
     stop("N needs to be a vector of integers.")
   }
-  if(any(length(s)>N)){print("Error: The population counts should not be exceeded by the sample counts.")}
   if(!is.null(seed))  set.seed(as.integer(seed))
-  prob=rep(0,K)
-  if(is.null(lbound)){
-   toN <- function(x,Ntot){
-     s <- 1+sum(exp(x))
-     Ntot*c(1,exp(x))/(1+sum(exp(x)))
-   }
-   tox <- function(N){
-     aaa <- log(N[-1]/N[1])
-     aaa[is.na(aaa) || is.infinite(aaa)] <- -1000
-     aaa
-   } 
-   llik <- function(x,Ntot,s){
-     llhoodf(N=toN(x,Ntot),s=s,verbosr=FALSE)
-   }
-   if(is.null(nstart)){
-          nstart=n/(1:K)
-          nstart=3*sum(n)*nstart/sum(nstart)
-   }
-   for(i in seq(along=N)){
-     out <- optim(par=tox(nstart), fn=llik, s=s, Ntot=N[i],
-      control=list(maxit=10000,fnscale=-1))
-     if(out$value > lbound || i ==1){
-       lbound <- out$value
-       Nmle <- toN(out$par, Ntot=N[i])
-     }
-   }
-  }else{
-   Nmle <- nstart
-  }
-  if(verbose) cat(paste("Rejection sampling bound set to",lbound,"\n"))
-  if(parallel==1){
-    Cret <- .C("bnw_mp",
-              N=as.integer(N),
-              lenN=as.integer(length(N)),
-              K=as.integer(K),
-              n=as.integer(length(s)),
-              s=as.integer(s),
-              nk=as.integer(n),
-              lbound=as.double(lbound),
-              dprob=as.double(prob),
-              prob=as.double(N),
-              NtotMLE=as.double(N),
-              Nprior=as.integer(prob),
-              Nmle=as.integer(round(Nmle)),
-              M=as.integer(M))
+
+  if(length(N)==1){
+      Cret <- margposN(s=s,N=N,K=K,prob=prob,M=M,seed=seed,n=n,
+                       return.all=return.all,parallel=parallel)
   }else{
     require(snow)
 #
@@ -95,202 +52,31 @@ margposteriorsize<-function(s,N=trunc(length(s)*seq(1.1,4,length=10)+1),
 #   Run the jobs with rpvm or Rmpi
 #
     flush.console()
-    MCsamplesize.parallel=round(M/parallel)
-    outlist <- clusterCall(cl, margposteriorsize,
-      s=s,N=N,K=K,n=n,dprob=prob,lbound=lbound,nstart=Nmle,
-      M=MCsamplesize.parallel)
-#   if(length(N)==0||!is.numeric(N)||trunc(N)!=N){
+    outlist <- clusterApply(cl, as.list(N), margposN,
+      s=s,K=K,prob=prob,M=M,n=n,return.all=FALSE,parallel=1)
 #
 #   Process the results
 #
-#   Cret <- 0
-#   for(i in (1 : parallel)){
-#    z <- outlist[[i]]
-#    Cret <- Cret+z/parallel
-#   }
-    np <- length(outlist)
-    Cret <- outlist[[1]]
-    Cret$M <- M
-    Cret$prob <- Cret$prob / np
-    for(i in 1:np){
-      out <- outlist[[i]]
-      Cret$prob <- Cret$prob + out$prob / np
-      if(out$lbound > Cret$lbound){
-        Cret$lbound <- out$lbound
-        Cret$Nmle <- out$Nmle
-      }
-    }
+    Cret <- list(unpos=unlist(outlist), N=Nrange, s=s,prob=prob,
+                 M=M,n=n)
     if(verbose){
-     cat("parallel samplesize=", parallel,"by", MCsamplesize.parallel,"\n")
+     cat("parallel samplesize=", parallel,"by", M,"\n")
     }
-#   }
     stopCluster(cl)
     if(getClusterOption("type")=="PVM") .PVM.exit()
   }
   if(return.all){
     Cret
   }else{
-    Cret$prob
+    Cret$unpos
   }
 }
-margposteriorsizewar<-function(s,N=trunc(length(s)*seq(1.1,4,length=10)+1),
-          K=(1:max(s)),
-          mu=5,rho=3,M=100000,
-          parallel=1,seed=NULL, verbose=TRUE, 
-          n=tabulate(s,nbin=K),
-          lbound=NULL,
-          nstart=NULL,
-          return.all=TRUE){
-  if(length(N)==0||!is.numeric(N)||trunc(N)!=N){
-    stop("N needs to be a vector of integers.")
-  }
-  if(any(length(s)>N)){print("Error: The population counts should not be exceeded by the sample counts.")}
-  if(!is.null(seed))  set.seed(as.integer(seed))
-  prob=rep(0,K)
-  if(is.null(lbound)){
-   toN <- function(x,Ntot){
-     s <- 1+sum(exp(x))
-     Ntot*c(1,exp(x))/(1+sum(exp(x)))
-   }
-   tox <- function(N){
-     aaa <- log(N[-1]/N[1])
-     aaa[is.na(aaa) || is.infinite(aaa)] <- -1000
-     aaa
-   } 
-   llik <- function(x,Ntot,s){
-     llhoodf(N=toN(x,Ntot),s=s,verbose=FALSE)
-   }
-   if(is.null(nstart)){
-          nstart=n/(1:K)
-          nstart=3*sum(n)*nstart/sum(nstart)
-   }
-   for(i in seq(along=N)){
-     out <- optim(par=tox(N[i]*nstart/sum(nstart)), fn=llik, 
-      Ntot=N[i], s=s,
-      control=list(maxit=10000,fnscale=-1))
-     if(out$value > lbound || i ==1){
-       lbound <- out$value
-       Nmle <- toN(out$par, Ntot=N[i])
-     }
-   }
-  }else{
-   Nmle <- nstart
-  }
-  if(verbose) cat(paste("Rejection sampling bound set to",lbound,"\n"))
-  if(parallel==1){
-    Cret <- .C("bnw_mpwar",
-              N=as.integer(N),
-              lenN=as.integer(length(N)),
-              K=as.integer(K),
-              n=as.integer(length(s)),
-              s=as.integer(s),
-              nk=as.integer(n),
-              lbound=as.double(lbound),
-              prob=as.double(N),
-              NtotMLE=as.double(N),
-              Nprior=as.integer(prob),
-              Nmle=as.integer(round(Nmle)),
-              mu=as.double(mu),
-              rho=as.double(rho),
-              M=as.integer(M))
-  }else{
-    require(snow)
-#
-#   Start PVM if necessary
-#
-#   setDefaultClusterOptions(type="PVM")
-    if(getClusterOption("type")=="PVM") {
-     if(verbose)
-     {
-      cat("Engaging warp drive using PVM ...\n")
-     }
-     require(rpvm)
-     PVM.running <- try(.PVM.config(), silent = TRUE)
-     if(inherits(PVM.running, "try-error"))
-     {
-      hostfile <- paste(Sys.getenv("HOME"), "/.xpvm_hosts", sep = "")
-      .PVM.start.pvmd(hostfile)
-      cat("no problem... PVM started by size...\n")
-     }
-    }else{
-     if(verbose)
-     {
-      cat("Engaging warp drive using MPI ...\n")
-     }
-    }
-#
-#   Start Cluster
-#
-    cl <- makeCluster(parallel)
-    clusterSetupRNG(cl)
-    clusterEvalQ(cl, library(size))
-#
-#   Run the jobs with rpvm or Rmpi
-#
-    flush.console()
-    MCsamplesize.parallel=round(M/parallel)
-    outlist <- clusterCall(cl, margposteriorsizewar,
-      s=s,N=N,K=K,mu=mu,rho=rho,n=n,lbound=lbound,nstart=Nmle,
-      M=MCsamplesize.parallel)
-#   if(length(N)==0||!is.numeric(N)||trunc(N)!=N){
-#
-#   Process the results
-#
-#   Cret <- 0
-#   for(i in (1 : parallel)){
-#    z <- outlist[[i]]
-#    Cret <- Cret+z/parallel
-#   }
-    np <- length(outlist)
-    Cret <- outlist[[1]]
-    Cret$M <- M
-    Cret$prob <- Cret$prob / np
-    for(i in 1:np){
-      out <- outlist[[i]]
-      Cret$prob <- Cret$prob + out$prob / np
-      if(out$lbound > Cret$lbound){
-        Cret$lbound <- out$lbound
-        Cret$Nmle <- out$Nmle
-      }
-    }
-    if(verbose){
-     cat("parallel samplesize=", parallel,"by", MCsamplesize.parallel,"\n")
-    }
-#   }
-    stopCluster(cl)
-    if(getClusterOption("type")=="PVM") .PVM.exit()
-  }
-  if(return.all){
-    Cret
-  }else{
-    Cret$prob
-  }
-}
-margpossize<-function(s,N=trunc(length(s)*seq(1.1,4,length=10)+1),
-          K=(1:max(s)),
-          mu=5,rho=3,M=100000,
-          parallel=1,seed=NULL, verbose=FALSE, 
-          n=tabulate(s,nbin=K),
-          return.all=FALSE){
-  if(length(N)==0||!is.numeric(N)||trunc(N)!=N){
-    stop("N needs to be a vector of integers.")
-  }
-  logprob <- N
-  for(i in seq(along=N)){
-   logprob[i] <- margposN(N=N[i],K=K,s=s,mu=mu,rho=rho,M=M,
-               parallel=parallel,seed=seed, verbose=verbose, 
-               n=n, return.all=return.all)
-   if(verbose) cat(paste("N=",N[i]," logprob=",logprob[i],"\n"))
-  }
-  list(N=N,logprob=logprob,s=s,K=K,mu=mu,rho=rho)
-}
-unposN<-function(mu,rho,N,K,s,n=tabulate(s,nbin=K),M=100000){
-    #this function takes a vector of population sizes and a vector s of 
-    #sequential sizes of sampled units and returns a log likelihood value
-    #s values must all be positive integers
-    if(any(length(s)>N)){print("Error: The population counts should not be exceeded by the sample counts.")}
-    prob=rep(0,K)
-    Cret <- .C("bnw_NC",
+unposN<-function(prob,N,K,s,n=tabulate(s,nbin=K),M=100000){
+  #this function takes a vector of population sizes and a vector s of 
+  #sequential sizes of sampled units and returns a log likelihood value
+  #s values must all be positive integers
+  if(length(s)>N){print("Error: The population counts should not be exceeded by the sample counts.")}
+  Cret <- .C("bnw_NC",
               N=as.integer(N),
               K=as.integer(K),
               n=as.integer(length(s)),
@@ -298,23 +84,61 @@ unposN<-function(mu,rho,N,K,s,n=tabulate(s,nbin=K),M=100000){
               nk=as.integer(n),
               Nk=as.integer(prob),
               prob=as.double(prob),
-              mu=as.double(mu),
-              rho=as.double(rho),
+              qprob=as.double(prob),
               M=as.integer(M),
               unpos=as.double(0))
     Cret$unpos
     Cret
 }
-margposN<-function(N,K,s,mu=5,rho=3,M=100000,
-          parallel=1,seed=NULL, verbose=FALSE, 
+margposN<-function(N, s,
+          K=max(s),
+          prob=rep(1/K,K),
+          M=100000,
+          parallel=1, seed=NULL, verbose=FALSE, 
           n=tabulate(s,nbin=K),
+	  qprob=NULL,
           return.all=FALSE){
   #this function takes a vector of population sizes and a vector s of 
   #sequential sizes of sampled units and returns a log likelihood value
   #s values must all be positive integers
-  if(any(length(s)>N)){print("Error: The population counts should not be exceeded by the sample counts.")}
+  if(length(s)>N){print("Error: The population counts should not be exceeded by the sample counts.")}
   if(!is.null(seed))  set.seed(as.integer(seed))
-  prob=rep(0,K)
+  toN <- function(x,Ntot,nk){
+    nk[nk!=0] <- (Ntot-sum(nk))*c(1,exp(x))/(1+sum(exp(x)))+nk[nk!=0]
+    nk
+  }
+  tox <- function(N,nk){
+    N <- N[nk!=0]-nk[nk!=0]
+    ltox <- log(N[-1]/N[1])
+    ltox[is.na(ltox) || is.infinite(ltox)] <- -100000
+    ltox
+  } 
+  llik <- function(x,Ntot,n,s,prob,M=1000){
+    qprob <- toN(x,Ntot,n)
+    qprob <- qprob/sum(qprob)
+    .C("bnw_NCbound",
+       N=as.integer(Ntot),
+       K=as.integer(length(qprob)),
+       n=as.integer(length(s)),
+       s=as.integer(s),
+       nk=as.integer(n),
+       Nk=as.integer(n),
+       prob=as.double(prob),
+       qprob=as.double(qprob),
+       M=as.integer(M),
+       unpos=as.double(0))$unpos
+  }
+  if(is.null(qprob)){
+   out <- N*n/sum(n)
+   out <- optim(par=tox(out,nk=n), fn=llik, 
+     Ntot=N, n=n, s=s, prob=prob,
+    control=list(maxit=1000,fnscale=-1))
+   Nmle <- toN(out$par, Ntot=N, nk=n)
+#  lbound <- llhoodf(N=Nmle, s=s, verbose=FALSE)
+#  qprob <- n/sum(n)
+   qprob <- Nmle/sum(Nmle)
+  }
+  print(qprob)
   if(parallel==1){
     Cret <- .C("bnw_NC",
               N=as.integer(N),
@@ -322,10 +146,9 @@ margposN<-function(N,K,s,mu=5,rho=3,M=100000,
               n=as.integer(length(s)),
               s=as.integer(s),
               nk=as.integer(n),
-              Nk=as.integer(prob),
+              Nk=as.integer(n),
               prob=as.double(prob),
-              mu=as.double(mu),
-              rho=as.double(rho),
+              qprob=as.double(qprob),
               M=as.integer(M),
               unpos=as.double(0))
   }else{
@@ -365,7 +188,7 @@ margposN<-function(N,K,s,mu=5,rho=3,M=100000,
     flush.console()
     MCsamplesize.parallel=round(M/parallel)
     outlist <- clusterCall(cl, margposN,
-      mu=mu,rho=rho,N=N,K=K,s=s,M=MCsamplesize.parallel)
+      s=s,N=N,K=K,prob=prob,M=MCsamplesize.parallel,n=n,qprob=qprob)
 #
 #   Process the results
 #
@@ -404,4 +227,195 @@ llhoodf<-function(N,s,n=tabulate(s,nbin=length(N)),verbose=TRUE){
               Nk=as.double(N),
               llik=as.double(0))
     Cret$llik
+}
+discretemleN<-function(N,s,
+          K=max(s),
+          M=100000,
+          parallel=1, seed=NULL, verbose=FALSE, 
+          n=tabulate(s,nbin=K),
+	  qprob=NULL,
+          return.all=TRUE){
+  if(length(s)>N){print("Error: The population counts should not be exceeded by the sample counts.")}
+  if(!is.null(seed))  set.seed(as.integer(seed))
+  toN <- function(x,Ntot,nk){
+    nk[nk!=0] <- (Ntot-sum(nk))*c(1,exp(x))/(1+sum(exp(x)))+nk[nk!=0]
+    nk
+  }
+  tox <- function(N,nk){
+    N <- N[nk!=0]-nk[nk!=0]
+    ltox <- log(N[-1]/N[1])
+    ltox[is.na(ltox) || is.infinite(ltox)] <- -100000
+    ltox
+  } 
+  llik <- function(x,Ntot,n,s){
+    N <- toN(x,Ntot,n)
+    .C("bnw_llik",
+              K=as.integer(length(N)),
+              n=as.integer(length(s)),
+              s=as.integer(s),
+              nk=as.integer(n),
+              Nk=as.double(N),
+              llik=as.double(0))$llik
+  }
+  if(is.null(qprob)){
+   out <- N*n/sum(n)
+   out <- optim(par=tox(out,nk=n), fn=llik, 
+     Ntot=N, n=n, s=s,
+    control=list(maxit=10000,fnscale=-1))
+   Nmle <- toN(out$par, Ntot=N, nk=n)
+   lbound <- llhoodf(N=Nmle, s=s, verbose=FALSE)
+   qprob <- Nmle/sum(Nmle)
+  }
+# print(qprob)
+# print(lbound)
+  if(parallel==1){
+    Cret <- .C("bnw_stocdiscrete",
+              N=as.integer(N),
+              K=as.integer(K),
+              n=as.integer(length(s)),
+              s=as.integer(s),
+              nk=as.integer(n),
+              Nk=as.integer(n),
+              qprob=as.double(qprob),
+              M=as.integer(M),
+              mllik=as.double(0))
+  }else{
+    require(snow)
+#
+#   Start PVM if necessary
+#
+#   setDefaultClusterOptions(type="PVM")
+    if(getClusterOption("type")=="PVM") {
+     if(verbose)
+     {
+      cat("Engaging warp drive using PVM ...\n")
+     }
+     require(rpvm)
+     PVM.running <- try(.PVM.config(), silent = TRUE)
+     if(inherits(PVM.running, "try-error"))
+     {
+      hostfile <- paste(Sys.getenv("HOME"), "/.xpvm_hosts", sep = "")
+      .PVM.start.pvmd(hostfile)
+      cat("no problem... PVM started by size...\n")
+     }
+    }else{
+     if(verbose)
+     {
+      cat("Engaging warp drive using MPI ...\n")
+     }
+    }
+#
+#   Start Cluster
+#
+    cl <- makeCluster(parallel)
+    clusterSetupRNG(cl)
+    clusterEvalQ(cl, library(size))
+#
+#   Run the jobs with rpvm or Rmpi
+#
+    flush.console()
+    MCsamplesize.parallel=round(M/parallel)
+    outlist <- clusterCall(cl, discretemleN,
+      s=s,N=N,K=K,M=MCsamplesize.parallel,n=n,qprob=qprob)
+#
+#   Process the results
+#
+    Cret <- outlist[[1]]
+    for(i in (2 : length(outlist))){
+     z <- outlist[[i]]
+     if(z$mllik < Cret$mllik){
+      Cret$Nk <- z$Nk
+      Cret$mllik <- z$mllik
+     }
+    }
+    if(verbose){
+     cat("parallel samplesize=", parallel,"by", MCsamplesize.parallel,"\n")
+    }
+    stopCluster(cl)
+    if(getClusterOption("type")=="PVM") .PVM.exit()
+  }
+  Cret$lbound <- lbound
+  if(return.all){
+    Cret
+  }else{
+    Cret$mllik
+  }
+}
+discretemle<-function(N=trunc(length(s)*seq(1.1,4,length=10)+1),
+          s,
+	  K=max(s),
+          M=100000,
+          parallel=1,seed=NULL, verbose=TRUE, 
+          n=tabulate(s,nbin=K),
+          return.all=TRUE){
+  if(length(N)==0||!is.numeric(N)||trunc(N)!=N){
+    stop("N needs to be a vector of integers.")
+  }
+  if(!is.null(seed))  set.seed(as.integer(seed))
+
+  if(length(N)==1){
+      Cret <- discretemleN(s=s,N=N,K=K,M=M,seed=seed,n=n,
+                return.all=return.all,parallel=parallel)
+  }else{
+    require(snow)
+#
+#   Start PVM if necessary
+#
+#   setDefaultClusterOptions(type="PVM")
+    if(getClusterOption("type")=="PVM") {
+     if(verbose)
+     {
+      cat("Engaging warp drive using PVM ...\n")
+     }
+     require(rpvm)
+     PVM.running <- try(.PVM.config(), silent = TRUE)
+     if(inherits(PVM.running, "try-error"))
+     {
+      hostfile <- paste(Sys.getenv("HOME"), "/.xpvm_hosts", sep = "")
+      .PVM.start.pvmd(hostfile)
+      cat("no problem... PVM started by size...\n")
+     }
+    }else{
+     if(verbose)
+     {
+      cat("Engaging warp drive using MPI ...\n")
+     }
+    }
+#
+#   Start Cluster
+#
+    cl <- makeCluster(parallel)
+    clusterSetupRNG(cl)
+    clusterEvalQ(cl, library(size))
+#
+#   Run the jobs with rpvm or Rmpi
+#
+    flush.console()
+    outlist <- clusterApply(cl, as.list(N), discretemleN,
+      s=s,K=K,M=M,n=n,return.all=TRUE,parallel=1)
+#
+#   Process the results
+#
+    Cret <- outlist[[1]]
+    Cret$N <- N
+    Cret$llik <- Cret$mllik
+    Cret$Nmle <- Cret$Nk
+    Cret$Nk <- NULL
+    for(i in (2 : length(outlist))){
+     z <- outlist[[i]]
+     Cret$llik <- c(Cret$llik,z$mllik)
+     Cret$lbound <- c(Cret$lbound,z$lbound)
+     if(z$mllik > Cret$mllik){
+      Cret$Nmle <- z$Nk
+      Cret$mllik <- z$mllik
+     }
+    }
+    stopCluster(cl)
+    if(getClusterOption("type")=="PVM") .PVM.exit()
+  }
+  if(return.all){
+    Cret
+  }else{
+    Cret$mllik
+  }
 }
