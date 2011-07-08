be6ec7be7a1d96ec2319b632f24781494527d20d
Mark S. Handcock <handcock@ucla.edu>
2011-07-08 17:12:20 +0000

    Increased the number of grid points for the local density estimation. In small bandwidth situations, it can run out of memory for storing the grid.  Explicitly, I increased the `maxk' argument to 500 from 100.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/rdsworkinggroup/software/size@596 adfe0e35-f9fe-4921-b9ec-9a706e04b580

--- R/size.utilities.R
+++ R/size.utilities.R
@@ -2,7 +2,7 @@ mode.density <- function(x,lbound=min(x),ubound=max(x), smooth=0.35, h=0){
 #     posdensN <- locfit(~ x, alpha=c(2*smooth,0.3))
 #     posdensN <- locfit(~ x)
 #     xp <- floor(lbound):ceiling(ubound)
-      posdensN <- locfit( ~ lp(x, nn=2*smooth, h=h))
+      posdensN <- locfit( ~ lp(x, nn=2*smooth, h=h, maxk=500))
       xp <- seq(lbound, ubound, length=10000)
       posdensN <- list(x=xp,y=predict(posdensN, newdata=xp))
       posdensN$x[which.max(posdensN$y)]
@@ -11,7 +11,7 @@ ll.density <- function(x,lbound=min(x),ubound=max(x), smooth=0.35, h=0){
 #     posdensN <- locfit(~ x, alpha=c(2*smooth,0.3))
 #     posdensN <- locfit(~ x)
 #     xp <- floor(lbound):ceiling(ubound)
-      posdensN <- locfit( ~ lp(x, nn=2*smooth, h=h))
+      posdensN <- locfit( ~ lp(x, nn=2*smooth, h=h, maxk=500))
       xp <- seq(lbound, ubound, length=10000)
       list(x=xp,y=predict(posdensN, newdata=xp))
 }
