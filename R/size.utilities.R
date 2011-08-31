22ebe85a672dbdbc7ea1ec39ed21f9c8ee3db598
Mark S. Handcock <handcock@ucla.edu>
2011-08-31 05:05:10 +0000

    Changed defaly mode computer back to the the simplier "density()" version in case the more sophisticated "locfit" version is the source of the crashes.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/rdsworkinggroup/software/size@646 adfe0e35-f9fe-4921-b9ec-9a706e04b580

--- R/size.utilities.R
+++ R/size.utilities.R
@@ -4,18 +4,18 @@ mode.density <- function(x,lbound=min(x,na.rm=TRUE),ubound=max(x,na.rm=TRUE), sm
 #     xp <- floor(lbound):ceiling(ubound)
       x <- x[!is.na(x)]
       if(length(x)==0){return(NA)}
-      posdensN <- try(locfit( ~ lp(x, nn=2*smooth, h=h, maxk=500)),silent=TRUE)
-      if(inherits(posdensN,"try-error")){
+#     posdensN <- try(locfit( ~ lp(x, nn=2*smooth, h=h, maxk=500)),silent=TRUE)
+#     if(inherits(posdensN,"try-error")){
        posdensN <- density(x, from=lbound, to=ubound)
-      }else{
-       xp <- seq(lbound, ubound, length=10000)
-       posdensN <- try(predict(posdensN, newdata=xp),silent=TRUE)
-       if(inherits(posdensN,"try-error")){
-        posdensN <- density(x, from=lbound, to=ubound)
-       }else{
-        posdensN <- list(x=xp,y=posdensN)
-       }
-      }
+#     }else{
+#      xp <- seq(lbound, ubound, length=10000)
+#      posdensN <- try(predict(posdensN, newdata=xp),silent=TRUE)
+#      if(inherits(posdensN,"try-error")){
+#       posdensN <- density(x, from=lbound, to=ubound)
+#      }else{
+#       posdensN <- list(x=xp,y=posdensN)
+#      }
+#     }
       posdensN$x[which.max(posdensN$y)]
 }
 ll.density <- function(x,lbound=min(x,na.rm=TRUE),ubound=max(x,na.rm=TRUE), smooth=0.35, h=0){
