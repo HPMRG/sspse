601d549666991456edbb5712fe53a65206ebbbb9
Mark S. Handcock <handcock@ucla.edu>
2013-04-12 04:29:07 +0000

    Minor renaming cleanup
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/rdsworkinggroup/software/size@845 adfe0e35-f9fe-4921-b9ec-9a706e04b580

--- R/cmp.natural.R
+++ R/cmp.natural.R
@@ -1,15 +1,17 @@
-cmp.natural <- function (mx,sdx,K=1000,guess=NULL) 
+cmp.natural <- function (mu,sig,K=1000,guess=NULL) 
 {
-#   natural is log(lambda), -nu
+# converts mean to natural
+# natural is log(lambda), -nu
+# natural is lambda, nu
     options(warn = -1)
     if(is.null(guess)){
-      guess = c(2*log(mx+0.25), log(2))
+      guess = c(2*log(mu+0.25), log(2))
     }
-    result = optim(par=guess, fn=function(p,mx,sdx,K) {
+    result = optim(par=guess, fn=function(p,mu,sig,K) {
         j <- 0:K
         a <- dcmp(x=j, lambda=exp(p[1]), nu=exp(p[2]), err=0.000001)
-	sqrt(abs(sum(a*j)^2-mx*mx)+abs(sum(a*j*j)-sdx*sdx-mx*mx))
-    }, mx=mx, sdx=sdx, K=K, method = "Nelder-Mead",
+	sqrt(abs(sum(a*j)^2-mu*mu)+abs(sum(a*j*j)-sig*sig-mu*mu))
+    }, mu=mu, sig=sig, K=K, method = "Nelder-Mead",
        control=list(maxit=20000))
     options(warn = 0)
     lambda = exp(result$par[1])
@@ -19,6 +21,7 @@ cmp.natural <- function (mx,sdx,K=1000,guess=NULL)
 }
 cmp.mu <- function (p,K=1000) 
 {
+# converts natural to mean
         j <- 0:K
         a <- dcmp(x=j, lambda=p[1], nu=p[2], err=0.000001)
 	mu <- sum(a*j)
