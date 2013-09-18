e8c66c1d64bc595c3fcfa426813c521b0d6bf76b
Mark S. Handcock <handcock@ucla.edu>
2013-09-18 20:25:51 +0000

    A new summary function to create a RichOutput plot of the prior and posterior results
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/rdsworkinggroup/software/size@992 adfe0e35-f9fe-4921-b9ec-9a706e04b580

--- R/summary.size.R
+++ R/summary.size.R
@@ -1,162 +1,46 @@
-summary.size <- function (object, ..., 
-                          digits = max(3, getOption("digits") - 3),
-                          correlation=FALSE, covariance=FALSE,
-                          eps=0.0001)
-{
-# separates out summary and print fns: MSH
-  if(any(is.na(object$coef)) & !is.null(object$mplefit)){
-     object$coef[is.na(object$coef)] <-
-     object$mplefit$coef[is.na(object$coef)]
-  }
-  if(is.null(object$hessian) && is.null(object$covar)){
-   return()
-  }
-  if(is.null(object$covar)){
-   asycov <- try(robust.inverse(-object$hessian), silent=TRUE)
-   if(inherits(asycov,"try-error")){
-    asycov <- diag(1/diag(-object$hessian))
-   }
-  }else{
-   asycov <- object$covar
-  }
-  rownames(asycov) <- names(object$coef)
-  colnames(asycov) <- rownames(asycov)
-  
-  asyse <- diag(asycov)
-  asyse[asyse<0|is.infinite(object$coef)|object$offset] <- NA
-  asyse <- sqrt(asyse)
-  if(any(is.na(asyse)&!object$offset) & !is.null(object$mplefit)){
-   if(is.null(object$mplefit$covar)){
-    mpleasycov <- try(robust.inverse(-object$mplefit$hessian), silent=TRUE)
-    if(inherits(mpleasycov,"try-error")){
-     mpleasycov <- diag(1/diag(-object$mplefit$hessian))
-    }
-   }else{
-    mpleasycov <- object$mplefit$covar
-   }
-   asyse[is.na(asyse)] <- sqrt(diag(mpleasycov))[is.na(asyse)]
-  }
-  asyse <- matrix(asyse, ncol=length(asyse))
-  colnames(asyse) <- colnames(asycov)
-  
-#   MSH changed to make clearer
-#   original <- format(object$MCMCtheta, digits = digits)
-#   original <- format(object$theta.original, digits = digits)
-
-  pseudolikelihood <- is.na(object$samplesize)
-  independence <- !is.null(object$theta1$independent) && all(object$theta1$independent)
-  ans <- list(formula=object$formula, randomeffects=object$re,
-              digits=digits, correlation=correlation,
-              degeneracy.value = object$degeneracy.value,
-              offset = object$offset,
-              drop = object$drop,
-              covariance=covariance,
-              pseudolikelihood=pseudolikelihood,
-              independence=independence,
-              iterations=object$iterations[1])
-
-  if(ans$pseudolikelihood){
-    ans$samplesize <- NA
-  }else{
-    ans$samplesize <-  object$samplesize
-  }
-    
-  if(!is.null(ans$randomeffects)){ 
-   if(!is.matrix(ans$randomeffects)){
-    ans$senderreceivercorrelation<-ans$randomeffects
-   }else{
-    corr <- ans$randomeffects[1,2]/sqrt(ans$randomeffects[1,1]*ans$randomeffects[2,2])
-    corr <- max(min(1,corr),-1)
-    ans$senderreceivercorrelation<-corr
-   }
-  }
-
-  nodes<- network.size(object$network)
-  dyads<- network.dyadcount(object$network)
-  if(!is.null(object$Z.mkl)){
-    p <- ncol(object$Z.mkl)
-  }else{
-    p <- 0
-  }
-  if(!is.null(object$cluster)){
-    df <- length(object$coef) + object$ngroups*(p+2) - 1 # ng-1 + ng *p + ng
-  }else{
-    df <- length(object$coef) + (nodes - (p + 1)/2) * p
-  }
-  rdf <- dyads - df
-  tval <- object$coef / asyse
-  pval <- 2 * pt(q=abs(tval), df=rdf, lower.tail=FALSE)
-
-# values <- format(object$coef,digits=digits)
-# names <- names(values)
-# names(values) <- NULL
-# casyse<-format(asyse, digits=digits)
-# cpval<-format(pval, digits=digits)
-# cmc.se <- format(object$mc.se,digits=digits)
-
-# cmc.se[object$offset] <- NA
-# cpval[object$offset]  <- NA
-# casyse[object$offset] <- NA
-
-#  count <- 1
-#  templist <- NULL
-#  while (count <= length(names))
-#   {
-#    templist <- append(templist,c(values[count],
-#         casyse[count],cpval[count],cmc.se[count]))
-#    count <- count+1
-#   }
+summary.size <- function(fit,support=1000,HPD.level=0.95){
+require(locfit)
+#require(coda)
+out <- fit$sample
+outN <- out[,"N"]
+#a=locfit( ~ lp(outN, nn=0.35, h=0, maxk=500))
+a=locfit( ~ lp(outN,nn=0.5))
+xp <- seq(fit$n,fit$maxN, length=support)
+posdensN <- predict(a, newdata=xp)
+posdensN <- support*posdensN / ((fit$maxN-fit$n)*sum(posdensN))
+# Next from coda
+#hpd <- HPDinterval(fit$sample[,"N"])[1:2]
+# MSH using locfit
+  cy <- cumsum(posdensN/sum(posdensN))
+  hpd <- c(xp[which.max(cy>((1-HPD.level)/2))],
+           xp[which.max(cy>((1+HPD.level)/2))])
+  if(is.na(hpd[1])) hpd[1] <- xp[1]
+  if(is.na(hpd[2])) hpd[2] <- xp[length(xp)]
 #
-#   tempmatrix <- matrix(templist, ncol=4,byrow=TRUE)
-#   tempmatrix[,c(1:2,4)] <- format(tempmatrix[,c(1:2,4)], digits=digits, 
-#                                   print.gap=2)
-#   tempmatrix[,3] <- format.pval(as.numeric(tempmatrix[,3]), digits = 3, eps= 1e-4)
-#   colnames(tempmatrix) <- c("estimate","s.e.","p-value","MCMC s.e.")
-#   rownames(tempmatrix) <- names
-
-  count <- 1
-  templist <- NULL
-  while (count <= length(names(object$coef)))
-    {
-     templist <- append(templist,c(object$coef[count],
-          asyse[count],object$mc.se[count],pval[count]))
-     count <- count+1
-    }
-
-  tempmatrix <- matrix(templist, ncol=4,byrow=TRUE)
-  colnames(tempmatrix) <- c("Estimate", "Std. Error", "MCMC s.e.", "p-value")
-  rownames(tempmatrix) <- names(object$coef)
-
-  devtext <- "Deviance:"
-  if (!independence) {
-    if (pseudolikelihood) {
-      devtext <- "Pseudo-deviance:"
-      ans$message <- "\nWarning:  The standard errors are based on naive pseudolikelihood and are suspect.\n"
-    } 
-    else if(any(is.na(object$mc.se))) {
-      ans$message <- "\nWarning:  The standard errors are suspect due to possible poor convergence.\n"
-    }
-  } else {
-    ans$message <- "\nFor this model, the pseudolikelihood is the same as the likelihood.\n"
-  }
-  ans$devtable <- c("",apply(cbind(paste(format(c("   Null", 
-            "Residual", ""), width = 8), devtext), 
-            format(c(object$null.deviance,
-                     -2*object$mle.lik, 
-                     object$null.deviance+2*object$mle.lik),
-                digits = 5), " on",
-            format(c(dyads, rdf, df),
-                digits = 5)," degrees of freedom\n"), 
-            1, paste, collapse = " "),"\n")
-
-  
-  ans$aic <- -2*object$mle.lik + 2*df
-  ans$bic <- -2*object$mle.lik + log(dyads)*df
-  
-  ans$coefs <- as.data.frame(tempmatrix)
-  ans$asycov <- asycov
-  ans$asyse <- asyse
-  class(ans) <- "summary.size"
-  ans
+  map <- xp[which.max(posdensN)]
+  mp <- sum(xp*posdensN)/sum(posdensN)
+  l90 <- xp[which.max(cy>0.9)]
+  l50 <- xp[which.max(cy>0.5)]
+#
+lpriorm <- exp(fit$lpriorm-max(fit$lpriorm))
+lpriorm <- lpriorm[fit$n+(1:length(lpriorm)) > fit$n & fit$n+(1:length(lpriorm)) < fit$maxN]
+lpriorm <- lpriorm / sum(lpriorm)
+cy <- cumsum(lpriorm)
+xp <- seq(fit$n,fit$maxN)
+pl90 <- xp[which.max(cy>0.9)]
+#
+#cat(sprintf("Prior:\nMean = %d, Median = %d, Mode = %d, 90%% = %d, 25%% = %d, 75%% = %d.\n",
+# round(fit$mean.prior.size), round(fit$median.prior.size), round(fit$mode.prior.size), round(pl90), round(fit$quartiles.prior.size[1]), round(fit$quartiles.prior.size[2])))
+#cat(sprintf("Posterior:\nMean = %d, Median = %d, MAP = %d, 90%% = %d, HPD = (%d, %d).\n",
+# round(mp),round(l50),round(map),round(l90),round(hpd[1]),round(hpd[2])))
+#
+res <- matrix(c(
+ round(fit$mean.prior.size), round(fit$median.prior.size), round(fit$mode.prior.size), round(pl90), round(fit$quartiles.prior.size[1]), round(fit$quartiles.prior.size[2]),
+ round(mp),round(l50),round(map),round(l90),round(hpd[1]),round(hpd[2])),byrow=TRUE,nrow=2)
+colnames(res) <- c("Mean","Median","Mode","90%","25%","75%")
+rownames(res) <- c("Prior","Posterior")
+res <- as.data.frame(res)
+attr(res, "heading") <- "Summary of Population Size Estimation"
+print.Anova(res)
+invisible(res)
 }
-
