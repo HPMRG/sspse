e1eff9fda6afee651f56597a39e67133328d15b8
Ian E. Fellows <ian.fellows@stat.ucla.edu>
2013-09-19 21:29:40 +0000

    factored out dialogs
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/rdsworkinggroup/software/size@1011 adfe0e35-f9fe-4921-b9ec-9a706e04b580

--- R/zzz.R
+++ R/zzz.R
@@ -19,45 +19,7 @@
 ######################################################################
 
 .onLoad <-function(libname, pkgname){
-	deducerNotLoaded <- try(.deducer == .jnull(),silent=TRUE)
-	if(inherits(deducerNotLoaded,"try-error") || deducerNotLoaded)
-		return(NULL)
-	
-  RDSAnalyst <- J("RDSAnalyst.RDSAnalyst")
-
   library.dynam("size", package=pkgname, lib.loc=libname)
-  
-  
-  deducer.addMenuItem("Prior Distribution",,".getDialog('Prior Distribution')$run()","RDS Population")
-  deducer.addMenuItem("Posterior Distribution",,".getDialog('Posterior Distribution')$run()","RDS Population")
-# commented out marginal dialogs for now (JC 9/13/13)
-#  deducer.addMenuItem("Marginal Posterior Distribution",,".getDialog('Marginal Posterior Distribution')$run()","RDS Population")
-#  deducer.addMenuItem("Marginal Posterior Size Dist.",,".getDialog('Marginal Posterior Size Dist.')$run()","RDS Population")
-  
-  
-  if(.windowsGUI){
-	  winMenuAddItem("RDS Data",'Prior Distribution',"deducer('Prior Distribution')")
-	  winMenuAddItem("RDS Data",'Posterior Distribution',"deducer('Posterior Distribution')")  
-#	  winMenuAddItem("RDS Data",'Marginal Posterior Distribution',"deducer('Marginal Posterior Distribution')")  
-#	  winMenuAddItem("RDS Data",'Marginal Posterior Size Dist.',"deducer('Marginal Posterior Size Dist.')")  
-	  
-  }
-  
-  else if(.jgr){	
-	  if(RDSAnalyst$isPro()){
-		  jgr.addMenuSeparator("RDS Population")
-		  jgr.addMenuItem("RDS Population","Prior Distribution","deducer('Prior Distribution')")
-		  jgr.addMenuItem("RDS Population","Posterior Distribution","deducer('Posterior Distribution')")
-#		  jgr.addMenuItem("RDS Population","Marginal Posterior Distribution","deducer('Marginal Posterior Distribution')")
-#		  jgr.addMenuItem("RDS Population","Marginal Posterior Size Dist.","deducer('Marginal Posterior Size Dist.')")
-		  
-	  }}
-	
-  .registerDialog("Prior Distribution", .makePriorDistribution)
-  .registerDialog("Posterior Distribution", .makePosteriorDistribution)
-#  .registerDialog("Marginal Posterior Distribution", .makeMarginalPosteriorDistribution)
-#  .registerDialog("Marginal Posterior Size Dist.", .makeMarginalPosteriorSize)
-  
 }
 
 .onAttach <- function(libname, pkgname){
