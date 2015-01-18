6b97d342ad028aead116a341d0fd07a7c819787c
Mark S. Handcock <handcock@ucla.edu>
2015-01-18 23:17:15 +0000

    Made K = max(network.size) the default. This must either be se manually or impute degrees should be used (so that the max is a good choice).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/rdsworkinggroup/software/size@1240 adfe0e35-f9fe-4921-b9ec-9a706e04b580

--- R/beginendparallel.R
+++ R/beginendparallel.R
@@ -1,61 +1,12 @@
 beginparallel<-function(parallel=1, type="MPI", seed=NULL, packagenames=c("size"),verbose=TRUE){
-    ### parallel is wrapper for MPI or PVM (mosix only has PVM)
-#   require(snow)
     require(parallel)
-#
-#   Start PVM if necessary
-#
-#   setDefaultClusterOptions(type="PVM")
-#   setDefaultClusterOptions(type="MPI")
-#   if(snow::getClusterOption("type")=="PVM"){
-    if(is.null(type)){
-     silentwarnings <- capture.output(try.rpvm<-suppressWarnings(require(rpvm, quietly=TRUE, warn.conflicts = FALSE)))
-     if(try.rpvm){
-      snow::setDefaultClusterOptions(type="PVM")
-      type <- "PVM"
-      if(verbose){
-       cat("Default warp drive is PVM ...\n")
-      }
-     }else{
-#     snow::setDefaultClusterOptions(type="MPI")
-      type <- "MPI"
-      if(verbose){
-       cat("Default warp drive is MPI ...\n")
-      }
-     }
-    }
-    if(type=="PVM"){
-     silentwarnings <- capture.output(try.rpvm<-suppressWarnings(require(rpvm, quietly=TRUE, warn.conflicts = FALSE)))
-     if(try.rpvm){
-      if(verbose){
-       cat("Engaging warp drive using PVM ...\n")
-      }
-#     require(rpvm)
-      PVM.running <- try(rpvm::.PVM.config(), silent = TRUE)
-      if(inherits(PVM.running, "try-error")){
-       hostfile <- paste(Sys.getenv("HOME"), "/.xpvm_hosts", sep = "")
-       if(file.exists(hostfile)){
-        rpvm::.PVM.start.pvmd(hostfile)
-       }else{
-        rpvm::.PVM.start.pvmd()
-       }
-       cat("no problem... PVM started...\n")
-      }
-     }else{
-      type <- "MPI"
-      if(verbose){
-       cat("PVM is not available. Engaging warp drive using MPI ...\n")
-      }
-     }
-    }else{
-     if(verbose){
-      cat("Engaging warp drive using MPI ...\n")
-     }
+    if(verbose){
+     cat("Engaging warp drive using MPI ...\n")
     }
 #
 #   Start Cluster
 #
-    ### Snow commands to set up cluster
+    ### Set up the cluster
     cl <- parallel::makeCluster(parallel,type=type)
     ### initialize parallel random number streams
     if(is.null(seed)){
@@ -68,30 +19,14 @@ beginparallel<-function(parallel=1, type="MPI", seed=NULL, packagenames=c("size"
       attached <- parallel::clusterCall(cl, require, package=pkg, character.only=TRUE)      
     }
 #
-#   Run the jobs with rpvm or Rmpi
+#   Run the jobs with Rmpi
 #
     ### make sure that R has printed out console messages before go parallel
     flush.console()
     return(cl)
 }
 endparallel<-function(cl, type="MPI", finalize=TRUE, verbose=TRUE){
-    ### stop cluster and PVM (in case PVM is flakey)
     parallel::stopCluster(cl)
-#   if(snow::getClusterOption("type")=="PVM"){rpvm::.PVM.exit()}
-#   if(finalize & snow::getClusterOption("type")=="MPI"){Rmpi::mpi.finalize()}
     if(finalize & type=="MPI"){Rmpi::mpi.finalize()}
-    if(finalize & type=="PVM"){rpvm::.PVM.exit()}
-#   if(type=="PVM"){
-#    if(require("rpvm",character.only = TRUE)){
-#     rpvm::.PVM.exit()
-#    }else{
-#     type <- "MPI"
-#    }
-#   }
-#   if(type=="MPI"){
-#    if(require("Rmpi",character.only = TRUE)){
-#     Rmpi::mpi.finalize()
-#    }
-#   }
     invisible()
 }
